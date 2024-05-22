b867e8be64b290dcb05b0da2001ef2c0710db31d
ecoledesmetiers <edm.dasco@gmail.com>
2024-05-22 15:44:01 +0200

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,145 @@
+var data = [{
+    "title": "Développer la motivation",
+    "questions": [
+        {
+            "title": "Prendre soin et reconnaître chacun",
+            "text": "Dans mes formations, je sais  prendre soin de mes stagiaires pour leur permettre de se créer une image positive d’eux même et de développer leur confiance en eux."
+        },
+        {
+            "title": "Donner du sens entre la formation et le projet ",
+            "text": "Dans mes formations, je veille  à donner du sens entre la formation et le projet de chaque stagiaire pour les aider à se représenter les buts de la formation, sa valeur ajoutée et les faire adhérer au parcours."
+        },
+        {
+            "title": "Sécuriser et valoriser les apprentissages",
+            "text": "Dans mes formations, je sais sécuriser et renforcer le potentiel des stagiaires en les encourageant et en les valorisant pour  les aider à développer leur confiance, dépasser les difficultés et assurer leur réussite."
+        },
+    ]},
+    {
+    "title": "Organiser la dynamique de groupe",
+    "questions": [
+        {
+            "title": "Mobiliser l’attention",
+            "text": "Dans mes formations, je sais mobiliser l’attention de mes stagiaires afin qu’ils restent vigilants et concentrés et mobilisent toutes leurs ressources."
+        },
+        {
+            "title": "Faciliter la prise de parole",
+            "text": "Dans les formations où j’interviens, je suis capable de favoriser la prise de parole des stagiaires en créant la confiance et en établissant un cadre de communication clair et positif dans le groupe."
+        },
+        {
+            "title": "Organiser la vie du groupe",
+            "text": "Dans mes formations, je suis en mesure d’organiser la vie du groupe de stagiaires  pour favoriser le climat général, renforcer la cohésion et leur sentiment d’appartenance au groupe."
+        },
+        {
+            "title": "Organiser le travail du groupe",
+            "text": "Dans mes formations, je sais organiser efficacement le travail de groupe de mes stagiaires, en utilisant  différentes méthodes et outils pédagogiques et en favorisant l’implication et la collaboration."
+        },
+    ]},
+    {
+    "title": "Réguler les comportements",
+    "questions": [
+        {
+            "title": "Adopter une posture éthique",
+            "text": "Dans mes formations, j’adopte  une posture éthique et exemplaire pour créer un climat de respect et d’intégrité dans le groupe."
+        },
+        {
+            "title": "Faciliter l’acceptation des règles",
+            "text": "Dans mes formations, Je sais faciliter l’acceptation des règles par des outils et méthodes et  je sais mettre en place un cadre sûr et partagé et un climat de confiance."
+        },
+        {
+            "title": "Réguler les attitudes inadaptées",
+            "text": "Quand cela est nécessaire, je sais gérer un comportement inadapté d’un stagiaire de manière graduelle pour assurer le respect des règles collectives partagées."
+        },
+    ]},
+    {
+    "title": "Individualiser la formation",
+    "questions": [
+        {
+            "title": "Caractériser chaque apprenant",
+            "text": "Dans mes groupes, je sais  caractériser mes apprenants en fonction de leur niveau,  de leur vitesse de travail, de leur styles d'apprentissage personnels et de leurs aptitudes particulières."
+        },
+        {
+            "title": "Adapter les activités aux besoins des apprenants",
+            "text": "Pour prendre en compte les différences dans un groupe de stagiaires, je sais organiser les apprentissages  par sous-groupes ou en autoformation en proposant des activités et des progressions  adaptées aux besoins de chacun."
+        },
+        {
+            "title": "Mobiliser l’expérience et ressources des stagiaires",
+            "text": "Au sein d'un groupe, je sais repérer les compétences et l’expérience des stagiaires et mobiliser celles-ci dans les apprentissages en donnant des rôles adaptés facilitant la collaboration."
+        },
+    ]},
+    {
+    "title": "Préparer les activités",
+    "questions": [
+        {
+            "title": "Scénariser les activités",
+            "text": "Pour mes formations, je sais  structurer et organiser les activités adaptées aux objectifs selon une progression et des règles permettant d'assurer une efficacité et une dynamique dans le groupe."
+        },
+        {
+            "title": "Anticiper les méthodes et les outils",
+            "text": "Pour mes formations, je connais  une grande variété d'outils et de méthodes que je peux mobiliser en fonction de ce que je connais de mes stagiaires et de la progression dans les objectifs visés."
+        },
+        {
+            "title": "Organiser l’espace",
+            "text": "Je connais les possibilité de l'environnement de formation et je sais comment organiser l'espace pour faciliter les activités individuelles et collectives que je veux proposer à mes stagiaires."
+        },
+    ]},
+    {
+    "title": "Soutenir l’apprentissage",
+    "questions": [
+        {
+            "title": "Diagnostiquer les difficultés",
+            "text": "Dans mes formations, je suis en mesure d’identifier avec pertinence la nature des difficultés d’un stagiaire et de repérer ses besoins spécifiques."
+        },
+        {
+            "title": "Relier les savoirs antérieurs avec la formation",
+            "text": "Je sais comment relier les connaissances antérieures et l’expérience des stagiaires avec les contenus de ma formation pour aider chacun à construire ses savoirs selon ses acquis."
+        },
+        {
+            "title": "Étayer les apprentissages",
+            "text": "Dans mes formations, je sais accompagner un stagiaire en difficulté, en repérant ses capacités et en lui apportant une aide adaptée pour favoriser ou consolider ses apprentissages."
+        },
+        {
+            "title": "Utiliser l’analyse réflexive",
+            "text": "Dans mes formations, je sais accompagner un stagiaire en difficulté à faire un travail réflexif sur son propre fonctionnement pour l’aider à avancer et à progresser."
+        },
+    ]},
+    {
+    "title": "Évaluer",
+    "questions": [
+        {
+            "title": "Choisir un type d’évaluation adapté",
+            "text": "Dans mes formations, je sais  choisir un type d’évaluation adapté, avec des outils et des modalités qui conviennent aux objectifs."
+        },
+        {
+            "title": "Conduire le processus d’évaluation",
+            "text": "Dans mes formations, je suis en mesure de conduire avec pertinence le processus d’évaluation en fonction de mes objectifs et des spécificités de mes stagiaires."
+        },
+        {
+            "title": "Apporter un feedback à l’apprenant",
+            "text": "Dans mes formations, je sais apporter un feed-back ciblé à chaque stagiaire pour les aider à progresser."
+        },
+        {
+            "title": "Préparer l’apprenant à un examen",
+            "text": "Dans mes formations, je suis en mesure de préparer correctement les stagiaires à un examen, en cas de besoin, en tenant compte de leurs difficultés et leurs appréhensions."
+        },
+    ]},
+    {
+    "title": "Piloter la formation",
+    "questions": [
+        {
+            "title": "Structurer et ajuster le scénario de la formation",
+            "text": "Dans mes formations, je suis en mesure d’organiser et d’ajuster le scénario de la formation en fonction de ce qui se passe individuellement et collectivement dans le groupe."
+        },
+        {
+            "title": "Organiser et ajuster le rythme de la formation",
+            "text": "Dans les formations où j’interviens, je maîtrise le rythme des apprentissages de la formation et je suis capable de l’adapter si nécessaire au groupe et aux individus."
+        },
+        {
+            "title": "Donner du sens aux apprentissages",
+            "text": "Dans les formations où j’interviens, je veille à ce que les stagiaires donnent du sens aux objectifs et aux contenus de la formation en adaptant mon accompagnement au rythme et à la progression de chacun."
+        },
+        {
+            "title": "Développer l’autonomie",
+            "text": "Dans les formations où j’interviens, je sais  développer l’autonomie des stagiaires en m’appuyant sur le potentiel de chacun et en impliquant certains stagiaires dans l’organisation de la formation."
+        },
+    ]},
+]
---
+++ readme.txt
@@ -0,0 +1,6 @@
+
+Textes à changer pour les différentes langues :
+
+ - index.html
+ - quizz.html
+ - assets/js/questions.js // json de toutes les questions
